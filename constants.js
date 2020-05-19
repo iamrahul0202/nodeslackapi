9e58c082e5da238e1427698e09035abd5ba4fd9a
rahul@off <rahul@off>
2020-05-19 23:14:13 +0530

    Added support for modal dialog

--- constants.js
+++ constants.js
@@ -1,5 +1,6 @@
 module.exports =  {
     appPort: 3000,
     signingKeySecret: '42d546cf786e98426e8305547638dd44',
+    token: 'xoxb-1138178812660-1130654795589-uevH6ROI68dl1FeN2IDA2Wqs',
     incomingWebhookUrl : "https://hooks.slack.com/services/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"
 }
--- interactivemessages.js
+++ interactivemessages.js
@@ -1,8 +1,11 @@
 const { createMessageAdapter } = require('@slack/interactive-messages');
 const constants = require('./constants')
 const { logger } = require('./logger')
+const { WebClient } = require('@slack/web-api');
+const modal = require('./modal')
 
 const slackSigningSecret = process.env.signingKeySecret || constants.signingKeySecret;
+const token = process.env.token || constants.token;
 const slackInteractions = createMessageAdapter(slackSigningSecret);
 
 slackInteractions.action({ type: 'static_select' }, (payload, respond) => {
@@ -16,6 +19,17 @@ slackInteractions.action({ type: 'button' }, (payload, respond) => {
     logger.info(payload)
     return { text: 'Processing...' };
 })
+
+
+slackInteractions.shortcut({ callback: 'reviewform', type: 'shortcut' }, (payload) => {
+    console.log(`triggerid ${payload.trigger_id}`)
+    return web.views.open({
+        token: token,
+        trigger_id: payload.trigger_id,
+        view: modal
+    })
+})
+
 module.exports = function (app, path) {
     app.use(path, slackInteractions.requestListener());
 }
