d1a4df380f97e6f66ca062317c83acbefda370b5
rahul@off <rahul@off>
2020-05-19 13:41:54 +0530

    Changes to read incomingwebhook url from request body

--- webhook.js
+++ webhook.js
@@ -3,14 +3,20 @@ const { IncomingWebhook } = require('@slack/webhook');
 const { logger } = require('./logger')
 
 
-const url = constants.incomingWebhookUrl;
+const url = process.env.webhookUrl || constants.incomingWebhookUrl;
+const webhook = new IncomingWebhook(url);
 
-const _webhook = new IncomingWebhook(url);
+
 
 module.exports = function (app, reqpath) {
     app.post(reqpath, (req, res) => {
+
         logger.info(req.body)
-        let webhookResponse = _webhook.send(req.body)
+        let webhookUrl = req.body.url
+        let webhookResponse = new IncomingWebhook(webhookUrl).send({
+            text: req.body.text
+        })
+
         webhookResponse.then((result) => {
             logger.info(result)
             res.status(200).send('Successfully triggered')
