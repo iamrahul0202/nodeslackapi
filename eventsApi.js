3c8b29291ef5de67c79ce44c95e867de19c35f2f
rahul@off <rahul@off>
2020-05-20 13:14:12 +0530

    Added UI samples. Triggering modal on click of button

--- eventsApi.js
+++ eventsApi.js
@@ -5,10 +5,22 @@ const { logger } = require('./logger')
 const slackSigningSecret = process.env.signingKeySecret || constants.signingKeySecret;
 const slackEvents = createEventAdapter(slackSigningSecret);
 
+const { WebClient } = require('@slack/web-api');
+const token = process.env.token || constants.token;
+const web = new WebClient(token);
 
 slackEvents.on('message', (event) => {
-    console.dir(event)
+    //console.dir(event)
     logger.info(event);
+
+    if(event.client_msg_id){
+        let ack = {
+            token: token,
+            text: `Received Message - ${event.text}`,
+            channel: payload.channel.id
+        }
+        web.chat.postMessage(ack)
+    }
 });
 
 module.exports = function (app, path) {
--- interactivemessages.js
+++ interactivemessages.js
@@ -3,11 +3,15 @@ const constants = require('./constants')
 const { logger } = require('./logger')
 const { WebClient } = require('@slack/web-api');
 
-const modal = require('./modal')
+const modal = require('./samples/modal')
+const modalshortcut = require('./samples/modalshortcut')
 
 const slackSigningSecret = process.env.signingKeySecret || constants.signingKeySecret;
 const token = process.env.token || constants.token;
-const slackInteractions = createMessageAdapter(slackSigningSecret);
+
+const slackInteractions = createMessageAdapter(slackSigningSecret, {
+    syncResponseTimeout: 4000
+});
 
 const web = new WebClient(token);
 
@@ -27,24 +31,29 @@ slackInteractions.action({ type: 'static_select' }, (payload, respond) => {
 slackInteractions.action({ type: 'button' }, (payload, respond) => {
     console.log('button')
     logger.info(payload)
-    console.dir(payload.actions[0].value)
+    console.dir(`Button clicked with value - ${payload.actions[0].value}`)
 
     let ack = {
         token: token,
-        text: `Button clicked with value - ${payload.actions[0].value}`,
+        text: `Thank You for acknowledgment. Here is the form....`,
         channel: payload.channel.id
     }
-
     web.chat.postMessage(ack)
+    return web.views.open({
+        token: token,
+        trigger_id: payload.trigger_id,
+        view: modal
+    })
+
 })
 
-
+//For handling shortcuts
 slackInteractions.shortcut({ callback: 'reviewform', type: 'shortcut' }, (payload) => {
     console.log(`triggerid ${payload.trigger_id}`)
     return web.views.open({
         token: token,
         trigger_id: payload.trigger_id,
-        view: modal
+        view: modalshortcut
     })
 })
 
