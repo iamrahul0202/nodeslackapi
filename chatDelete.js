735e34b1cf25fcc4a52214be1152461e0c6bc66c
rahul@off <rahul@off>
2020-05-20 18:37:30 +0530

    added delete chat script(This should be used only from local)

---
+++ chatDelete.js
@@ -0,0 +1,39 @@
+const constants = require('./constants')
+const { WebClient } = require('@slack/web-api');
+const token = process.env.token || constants.token;
+const web = new WebClient(token);
+const channelId = process.env.channelId || 'D014KRM0RRN'
+
+async function deleteChat(msg) {
+    return await web.chat.delete({
+        token: constants.adminToken,
+        channel: channelId,
+        ts: msg.ts
+    }).catch(err => console.log(`err.... ${err.data.error}`))
+}
+
+function clean() {
+    //fetching all conversations for a channel
+
+    web.conversations.history({
+        token: constants.botToken,
+        channel: channelId
+    }).then(res => {
+        let promises = [];
+        for (const msg of res.messages) {
+            promises.push(deleteChat(msg))
+        }
+
+        return Promise.all(promises)
+
+    }).then((r) => {
+        console.dir(r)
+        console.log('Successfully deleted all the chats')
+    }).catch((err) => {
+        console.log('catch....')
+        console.dir(err)
+    })
+}
+
+
+clean()
--- constants.js
+++ constants.js
@@ -1,6 +1,8 @@
 module.exports =  {
     appPort: 3000,
     signingKeySecret: '42d546cf786e98426e8305547638dd44',
-    token: 'xoxb-1138178812660-1130654795589-uevH6ROI68dl1FeN2IDA2Wqs',
+    adminToken: 'xoxp-1138178812660-1117257727847-1135379541298-94cac57ae312052c4288b40e193965f9',
+    botToken: 'xoxb-1138178812660-1130654795589-Sbo4QoB8tYj5DfKurUY3nSPm',
+    token: this.adminToken,
     incomingWebhookUrl : "https://hooks.slack.com/services/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"
 }
