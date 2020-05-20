b2544a391e72498e3cd63e6ad2929858d0399d12
rahul@off <rahul@off>
2020-05-20 19:48:08 +0530

    Added user token for deletingchat

--- chatDelete.js
+++ chatDelete.js
@@ -2,11 +2,11 @@ const constants = require('./constants')
 const { WebClient } = require('@slack/web-api');
 const token = process.env.token || constants.token;
 const web = new WebClient(token);
-const channelId = process.env.channelId || 'D014KRM0RRN'
+const channelId = process.env.channelId || 'D014KRM0RRN' //D014KRM0RRN  C0145EMSSEQ
 
 async function deleteChat(msg) {
     return await web.chat.delete({
-        token: constants.adminToken,
+        token: constants.botToken,
         channel: channelId,
         ts: msg.ts
     }).catch(err => console.log(`err.... ${err.data.error}`))
@@ -16,7 +16,7 @@ function clean() {
     //fetching all conversations for a channel
 
     web.conversations.history({
-        token: constants.botToken,
+        token: constants.adminToken,
         channel: channelId
     }).then(res => {
         let promises = [];
--- constants.js
+++ constants.js
@@ -1,8 +1,8 @@
 module.exports =  {
     appPort: 3000,
     signingKeySecret: '42d546cf786e98426e8305547638dd44',
-    adminToken: 'xoxp-1138178812660-1117257727847-1135379541298-94cac57ae312052c4288b40e193965f9',
-    botToken: 'xoxb-1138178812660-1130654795589-Sbo4QoB8tYj5DfKurUY3nSPm',
+    adminToken: 'xoxp-1138178812660-1117257727847-1134129880085-9a1fc5a3e7e8fd00d04810b98af0cf62',
+    botToken: 'xoxb-1138178812660-1130654795589-0ZqdX4EdlGerZprTQkTfwEA0',
     token: this.adminToken,
     incomingWebhookUrl : "https://hooks.slack.com/services/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"
 }
